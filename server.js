67108d897f6af9fea2715cdb1af92d4f9dfb0a8a
Nate Naranjo <naranjo_nathaniel@live.com>
2022-11-29 13:04:09 -0600

    Created the server js files

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const db = mysql.createConnection (
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database`)
+);
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
